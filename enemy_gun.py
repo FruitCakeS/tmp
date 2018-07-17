450e704f8e19c668ba62d98083163575053872e9
Sean Lu <seanlu@fb.com>
2018-07-16 17:56:19 -0700

    3.0

--- enemy_gun.py
+++ enemy_gun.py
@@ -1,7 +1,7 @@
 def distance(x1,y1,x2,y2):
 	return ((x1-x2)**2 + (y1-y2)**2)**0.5
 
-print distance(0,3,4,0)
+print(distance(0,3,4,0))
 
 def enemy_gun(x1,y1,x2,y2):
 	if distance(x1,y1,x2,y2) <=10:
@@ -9,4 +9,4 @@ def enemy_gun(x1,y1,x2,y2):
  	else:
 		return "sniper_rifle"
 
-print enemy_gun(0,3,4,0)
+print(enemy_gun(0,3,4,0))
